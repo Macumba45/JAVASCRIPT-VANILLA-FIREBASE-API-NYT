9c6e254c8d5862950f702c66893c243e00697152
Macumba45 <gonzalolovo@gmail.com>
2022-12-19 02:08:58 +0100

    Subida mejorada CSS

--- Services/api.js
+++ Services/api.js
@@ -30,6 +30,13 @@ const fetchListBooks = async () => {
 
 const getBookDetails = async (listName) => {
 
+    const header = document.getElementById('listbook')
+    const headerH1 = document.createElement('h1')
+    headerH1.setAttribute("id", "listbook")
+    const headertitle = listName
+    headerH1.innerText = headertitle
+    header.appendChild(headerH1)
+
     try {
 
         const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${listName}.json?api-key=KF9ASDfmvWA3uXGbVU3FIPT5iWhQoPsB`)
