19c55a43dd8975ae17d5158fca0aebea8a7ffeaa
Macumba45 <gonzalolovo@gmail.com>
2022-12-20 19:23:16 +0100

    Diseño Login

---
+++ Services/firebaseAuth.js
@@ -0,0 +1,10 @@
+let userLogin = () => {
+
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    console.log("email=" + ' ' + email + ' ' + "password=" + ' ' + password)
+
+}
+
+
+
