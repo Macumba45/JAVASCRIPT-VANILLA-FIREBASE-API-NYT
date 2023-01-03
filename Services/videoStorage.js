414903eb93d26492a8f1123281bd3b7a13756416
Macumba45 <gonzalolovo@gmail.com>
2023-01-03 01:12:03 +0100

    cacheStorage1.2

--- Services/videoStorage.js
+++ Services/videoStorage.js
@@ -1,15 +1,20 @@
 if (window.caches) {
+
     caches.open('videoIndex')
 
     caches.open('videoIndex').then(cache => {
 
         cache.addAll([
 
-
             '../Img/Untitled.mp4',
             './index.html',
             '../Img/79-790806_new-york-times-logo-new-york-times-png.png',
-            '../Services/main.js'
+            '../Services/loginForm.js',
+            '../Services/registerForm.js',
+            '../Services/switchForm.js',
+            '../Services/signUpForm.js',
+            '../Services/loginUpForm.js',
+
 
         ])
 
